d6c5578cfa11b5ee7306566db2587d19c2516163
Judeukana@gmail.com <judeukana@Judes-MacBook-Pro.local>
2023-12-13 09:07:49 +0000

    added top-nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './auth/login/login.component';
 import { SideNavComponent } from './app-core/common/side-nav/side-nav.component';
+import { TopNavComponent } from './app-core/common/top-nav/top-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,FormsModule,LoginComponent,SideNavComponent],
+  imports: [CommonModule, RouterOutlet,FormsModule,LoginComponent,SideNavComponent,TopNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
